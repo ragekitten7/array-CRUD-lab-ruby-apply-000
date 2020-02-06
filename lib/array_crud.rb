a64afc439e7bd4dc4747ec5d2c194a287323fcdb
Sam Bryant <bryantsj@gmail.com>
2020-02-06 14:56:15 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -26,7 +26,8 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
-
+  array = [1,2,3,4]
+  array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
